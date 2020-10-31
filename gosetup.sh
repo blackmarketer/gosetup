9876a1f6a8fec313453cbcb6aafb46d3b7311d05
Alan Abhilash <48115305+blackmarketer@users.noreply.github.com>
2020-10-31 15:28:51 +0530

    Add files via upload

---
+++ gosetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+go_setup(){
+
+wget -q https://golang.org/dl/go1.15.3.linux-amd64.tar.gz 
+
+sudo tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz > /dev/null
+
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+
+mkdir $HOME/go
+
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=$PATH:$GOPATH/bin" >> ~/.bashrc
+
+go version
+echo "go setup complete....."
+
+}
+go_setup
